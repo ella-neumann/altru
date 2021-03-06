b60fff6753cc568ceb193fd25e73f5b1a08e3d67
drazicc <drazicc@wwu.edu>
2021-03-05 17:02:18 -0800

    adding an actual display of survey results

--- client/src/app/views/Survey/SurveyPage4.jsx
+++ client/src/app/views/Survey/SurveyPage4.jsx
@@ -19,8 +19,12 @@ const useStyles = makeStyles((theme) => ({
     display: "none",
   },
 }));
-
-const SurveyPage4 = () => {
+//16. You have noticed that the call to action for climate change has fallen short of your expectations and want to do more.
+//17. You are often concerned about ocean life and want to help reduce our negative impacts on the ocean.
+//18. You are often concerned about life on land and want to help reduce our negative impacts on the affected ecosystems.
+//19. You want to help improve peace and social justice, at all levels, locally or around the world.
+//20. You want to get involved with each of the implimentations of the Sustainable Development Goals.
+const Surveypage4 = () => {
   const classes = useStyles();
   return (
     <div className="m-sm-30">
@@ -28,62 +32,133 @@ const SurveyPage4 = () => {
         <Breadcrumb
           routeSegments={[
             { name: "Survey", path: "/Survey" },
-            { name: "Page 4" },
+            { name: "Page 4/4" },
           ]}
         />
       </div>
-      <SimpleCard>
-      <Table>
-          <TableHead>
-            <TableRow className="bg-default" style={{justifyContent: "space-evenly" }}>
-              <TableCell className="px-0" colspan="9"></TableCell>
-              <TableCell className="px-0" colspan="2">Strongly Disagree </TableCell>
-              <TableCell className="px-0" colspan="2">     Disagree     </TableCell>
-              <TableCell className="px-0" colspan="2">     Neutral      </TableCell>
-              <TableCell className="px-0" colspan="2">      Agree       </TableCell>
-              <TableCell className="px-0" colspan="2">  Strongly Agree  </TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            <TableRow style={{justifyContent: "space-evenly" }}>
-              <TableCell className="px-0" colspan="8"><b><font size="4">16. You have noticed that the call to action for climate change has fallen short of your expectations and want to do more.</font></b></TableCell>
-              <TableCell className="px-0" colspan="11"><LikertButtons questionId="q-16" /></TableCell>
-            </TableRow>
-            <TableRow style={{justifyContent: "space-evenly" }}>
-              <TableCell className="px-0" colspan="8"><b><font size="4">17. You are often concerned about ocean life and want to help reduce our negative impacts on the ocean.</font></b></TableCell>
-              <TableCell className="px-0" colspan="11"><LikertButtons questionId="q-17" /></TableCell>
-            </TableRow>
-            <TableRow style={{justifyContent: "space-evenly" }}>
-              <TableCell className="px-0" colspan="8"><b><font size="4">18. You are often concerned about life on land and want to help reduce our negative impacts on the affected ecosystems.</font></b></TableCell>
-              <TableCell className="px-0" colspan="11"><LikertButtons questionId="q-18" /></TableCell>
-            </TableRow>
-            <TableRow style={{justifyContent: "space-evenly" }}>
-              <TableCell className="px-0" colspan="8"><b><font size="4">19. You want to help improve peace and social justice, at all levels, locally or around the world.</font> </b></TableCell>
-              <TableCell className="px-0" colspan="11"><LikertButtons questionId="q-19" /></TableCell>
-            </TableRow>
-            <TableRow style={{justifyContent: "space-evenly" }}>
-              <TableCell className="px-0" colspan="8"><b><font size="4">20. You want to get involved with each of the implimentations of the Sustainable Development Goals.</font></b></TableCell>
-              <TableCell className="px-0" colspan="11"><LikertButtons questionId="q-20" /></TableCell>
-            </TableRow>
-          </TableBody>
-        </Table>
-      </SimpleCard>
-      <Button
-        variant="contained"
-        color="primary"
-        onClick={() => {
-          window.location.href = "../surveyresults";
-        }}
+      <div>
+        <SimpleCard>
+          <Table>
+            <TableHead>
+              <TableRow
+                className="bg-default"
+                style={{ justifyContent: "space-evenly" }}
+              >
+                <TableCell className="px-0" colspan="9"></TableCell>
+                <TableCell className="px-0" colspan="2">
+                  Strongly Disagree{" "}
+                </TableCell>
+                <TableCell className="px-0" colspan="2">
+                  {" "}
+                  Disagree{" "}
+                </TableCell>
+                <TableCell className="px-0" colspan="2">
+                  {" "}
+                  Neutral{" "}
+                </TableCell>
+                <TableCell className="px-0" colspan="2">
+                  {" "}
+                  Agree{" "}
+                </TableCell>
+                <TableCell className="px-0" colspan="2">
+                  {" "}
+                  Strongly Agree{" "}
+                </TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              <TableRow style={{ justifyContent: "space-evenly" }}>
+                <TableCell className="px-0" colspan="8">
+                  <b>
+                    <font size="4">
+                      16. You have noticed that the call to action for climate change 
+                      has fallen short of your expectations and want to do more.
+                    </font>
+                  </b>
+                </TableCell>
+                <TableCell className="px-0" colspan="11">
+                  <LikertButtons questionId="q-1" />
+                </TableCell>
+              </TableRow>
+              <TableRow style={{ justifyContent: "space-evenly" }}>
+                <TableCell className="px-0" colspan="8">
+                  <b>
+                    <font size="4">
+                      17. You are often concerned about ocean life 
+                      and want to help reduce our negative impacts on the ocean.
+                    </font>
+                  </b>
+                </TableCell>
+                <TableCell className="px-0" colspan="11">
+                  <LikertButtons questionId="q-2" />
+                </TableCell>
+              </TableRow>
+              <TableRow style={{ justifyContent: "space-evenly" }}>
+                <TableCell className="px-0" colspan="8">
+                  <b>
+                    <font size="4">
+                      18. You are often concerned about life on land and 
+                      want to help reduce our negative impacts on the affected ecosystems.
+                    </font>
+                  </b>
+                </TableCell>
+                <TableCell className="px-0" colspan="11">
+                  <LikertButtons questionId="q-3" />
+                </TableCell>
+              </TableRow>
+              <TableRow style={{ justifyContent: "space-evenly" }}>
+                <TableCell className="px-0" colspan="8">
+                  <b>
+                    <font size="4">
+                      19. You want to help improve peace and social justice, 
+                      at all levels, locally or around the world.
+                    </font>
+                  </b>
+                </TableCell>
+                <TableCell className="px-0" colspan="11">
+                  <LikertButtons questionId="q-4" />
+                </TableCell>
+              </TableRow>
+              <TableRow style={{ justifyContent: "space-evenly" }}>
+                <TableCell className="px-0" colspan="8">
+                  <b>
+                    <font size="4">
+                      20. You want to get involved with each of 
+                      the implimentations of the Sustainable Development Goals.
+                    </font>
+                  </b>
+                </TableCell>
+                <TableCell className="px-0" colspan="11">
+                  <LikertButtons questionId="q-5" />
+                </TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </SimpleCard>
+      </div>
+
+      <div
         style={{
-          position: "fixed",
-          minWidth: "10%",
-          bottom: 200,
-          right: 750
+          position: "absolute",
+          margin: "20px",
+          right: "15px",
         }}
-        >Submit
-      </Button>
+      >
+        <Button
+          variant="contained"
+          color="primary"
+          style={{
+            width: "150px",
+          }}
+          onClick={() => {
+            window.location.href = "../surveyresults";
+          }}
+        >
+          Next
+        </Button>
+      </div>
     </div>
   );
 };
 
-export default SurveyPage4;
+export default Surveypage4;
---
+++ server/routes/surveyresults.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var axios = require('axios');
+var router = express.Router();
+
+/* GET survey results. */
+router.get('/', function(req, res, next) {
+  axios.get("https://api.data.charitynavigator.org/v2/Organizations?app_id=700cb4b4&app_key=34dd0899d430f0b4d70a8d365cadb1bc&rated=true")
+    .then(response => {
+        res.json(response.data);
+    })
+    .catch(error => {
+        console.log(error);
+    });
+});
+
+module.exports = router;
